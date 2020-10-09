2811a2319be1fe0f83ea6ca7e917188ab8c0d7b8
colbyfayock <fayock@gmail.com>
2020-10-08 20:23:02 -0400

    Step 1: adding products and displaying them in the app

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import products from '../products.json';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -20,33 +22,19 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {products.map(product => {
+            return (
+              <div key={product.id} className={styles.card}>
+                <img src={product.image} alt={`Preview of ${product.title}`} />
+                <h3>{ product.title }</h3>
+                <p>{ product.description }</p>
+                <p>${ product.price }</p>
+                <p>
+                  <button>Add to Cart</button>
+                </p>
+              </div>
+            );
+          })}
         </div>
       </main>
 
