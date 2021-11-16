d65b8ed6c21e93f59f0e18f508b83fe548e5e292
Ludovic Armand <77077373+ludovic-armand@users.noreply.github.com>
2021-11-16 15:26:19 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,19 @@
 import '../styles/globals.css'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router'
+
+function useResetHistory() {
+  const router = useRouter()
+
+  useEffect(() => {
+    document.addEventListener("snipcart.ready", () => {
+      Snipcart.events.on('snipcart.initialized', (snipcartState) => {
+        // use `router.asPath` instead of `router.pathname`
+        router.replace(router.asPath)
+      });
+    });
+  }, [])
+}
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
